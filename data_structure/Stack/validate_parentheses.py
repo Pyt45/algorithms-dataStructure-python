f37b6ff86a59677f2445fbcddf9f4b3febe40f05
pyt45 <ayoub1234aqlzim@gmail.com>
2022-02-27 15:54:52 +0100

    validate parentheses

--- data_structure/Stack/validate_parentheses.py
+++ data_structure/Stack/validate_parentheses.py
@@ -4,5 +4,15 @@ from typing import Dict
 def validate_parentheses(expression: str) -> bool:
     pairs: Dict[str, str] = { '(': ')', '[': ']', '{': '}' }
     s: list[str] = []
-    
-    pass
+    for i in range(len(expression)):
+        s.append(expression[i])
+        if len(s) >= 2 and pairs.get(s[-2]) == s[-1]:
+            s.pop()
+            s.pop()
+    return len(s) == 0
+
+if __name__ == '__main__':
+    print(validate_parentheses("[]{}({[]}){"))
+    print(validate_parentheses("["))
+    print(validate_parentheses("[]{}({[]})"))
+    print(validate_parentheses("[{(())}]"))
